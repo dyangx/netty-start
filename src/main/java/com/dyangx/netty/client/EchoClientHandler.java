d5640c3989f668cf387d77c9e898bbf82856c235
dyangx <yaabbccj@163.com>
2019-10-07 21:59:49 +0800

    init

---
+++ src/main/java/com/dyangx/netty/client/EchoClientHandler.java
@@ -0,0 +1,13 @@
+package com.dyangx.netty.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+
+    }
+}
